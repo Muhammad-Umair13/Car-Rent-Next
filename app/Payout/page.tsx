3af944e93e9b7660bda72dd75d2fb92398cae174
Muhammad Umair <160301486+Muhammad-Umair13@users.noreply.github.com>
2024-12-14 21:08:07 +0500

    edit changes

---
+++ app/Payout/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Payment = () => {
+  return (
+    <div className="w-full h-[2240px] bg-[#F6F7F9] flex">
+        <div>
+            <div></div>
+            <div>payment2</div>
+            <div>payment3</div>
+            <div>payment4</div>
+        </div>
+        <div>rental summary</div>
+    </div>
+  )
+}
+
+export default Payment
