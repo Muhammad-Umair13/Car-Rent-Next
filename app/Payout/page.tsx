bc668cab651d7f7b5be3952db98368ad363539d9
Muhammad Umair <160301486+Muhammad-Umair13@users.noreply.github.com>
2024-12-14 21:36:43 +0500

    edit changes

--- app/Payout/page.tsx
+++ app/Payout/page.tsx
@@ -3,8 +3,29 @@ import React from 'react'
 const Payment = () => {
   return (
     <div className="w-full h-[2240px] bg-[#F6F7F9] flex">
-        <div>
-            <div></div>
+        <div className="w-[852px] h-[2176] mt-8 ml-8 gap-8">
+            <div className="h-[226px] bg-white">
+                <div>
+                <p className="font-bold text-xl">Billing Info</p>
+                <p className="font-medium text-sm">Please enter your billing info</p>
+                </div>
+
+                <div>
+                    <form>
+                        <label for="name">Name</label>
+                        <input type="text" id="name" required className="w-[386px] h-14 rounded-xl" placeholder='Enter Your Name'></input>
+
+                        <label></label>
+                        <input></input>
+
+                        <label></label>
+                        <input></input>
+
+                        <label></label>
+                        <input></input>
+                    </form>
+                </div>
+            </div>
             <div>payment2</div>
             <div>payment3</div>
             <div>payment4</div>
