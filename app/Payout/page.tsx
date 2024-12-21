fc5aec33cf1e00b991d1cb4c5168cad229e3cbf9
Muhammad Umair <160301486+Muhammad-Umair13@users.noreply.github.com>
2024-12-21 17:49:40 +0500

    edit changes

--- app/Payout/page.tsx
+++ app/Payout/page.tsx
@@ -4,8 +4,9 @@ const Payment = () => {
     return (
         <div className="w-full h-[2240px] bg-[#F6F7F9] flex">
             <div className="w-[852px] mt-8 ml-8 gap-8">
-                <div className="bg-white h-[336px]">
-                    <div>
+
+                <div className="bg-white h-[336px] rounded-[10px]">
+                    <div className="ml-6 pt-4">
                         <p className="font-bold text-xl">Billing Info</p>
                         <p className="font-medium text-sm">Please enter your billing info</p>
                     </div>
@@ -37,8 +38,9 @@ const Payment = () => {
                         </form>
                     </div>
                 </div>
-                <div className="w-[852px] h-[664px] rounded-xl mt-8 bg-white">
-                    <div>
+
+                <div className="w-[852px] h-[664px] mt-8 bg-white rounded-[10px]">
+                    <div className="ml-6 pt-4">
                         <p className="font-bold text-xl">Rental Info</p>
                         <p className="font-medium text-sm">Please select your rental date</p>
                     </div>
@@ -106,7 +108,49 @@ const Payment = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col items-center justify-center py-16 px-3 md:px-0">
+                <div className="h-[596px] bg-[#FFFFFF] mt-8 rounded-[10px]">
+                    <div className="ml-6 pt-4">
+                        <p className="font-bold text-xl">Payment Method</p>
+                        <p className="font-medium text-sm">Please enter your payment method</p>
+                    </div>
+                    <div className="h-[308px] w-[804px] bg-[#F6F7F9] ml-6 rounded-[10px] mt-20">
+                        <div>
+                            <input type="radio" className="mt-8 ml-6 font-semibold text-base" /> Credit Card
+                            {/* image */}
+                        </div>
+
+                        <div>
+                        <form>
+                            <div className="flex">
+                                <div className="mt-5 ml-6 ">
+                                    <label for="name" className="font-semibold text-base">Card Number</label><br />
+                                    <input type="text" id="name" required className="w-[356px] h-14 rounded-xl bg-white p-5" placeholder='Card number'></input>
+                                </div>
+
+                                <div className="mt-5 ml-6">
+                                    <label for="name" className="font-semibold text-base">Expration Date</label><br />
+                                    <input type="text" id="name" required className="w-[356px] h-14 rounded-xl bg-white p-5"></input>
+                                </div>
+                            </div>
+                            <div className="flex">
+                                <div className="mt-5 ml-6 ">
+                                    <label for="name" className="font-semibold text-base">Card Holder</label><br />
+                                    <input type="text" id="name" required className="w-[356px] h-14 rounded-xl bg-white p-5" placeholder='Card holder'></input>
+                                </div>
+
+                                <div className="mt-5 ml-6">
+                                    <label for="name" className="font-semibold text-base">CVC</label><br />
+                                    <input type="text" id="name" required className="w-[356px] h-14 rounded-xl bg-white p-5" placeholder='CVC'></input>
+                                </div>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>option1</div>
+                    <div>option2</div>
                 </div>
 
                 <div>payment4</div>
